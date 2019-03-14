a8cd4367b488a656d4009c825bc2b994e4cfecde
pchasapis <pantelis.chasapis@atcom.gr>
2019-03-14 14:50:46 +0200

    -- adding recognize greek

--- app/src/main/java/gr/padpad/dialogflow/MainActivity.java
+++ app/src/main/java/gr/padpad/dialogflow/MainActivity.java
@@ -143,6 +143,8 @@ public class MainActivity extends AppCompatActivity {
     private void startSpeechRecognizer() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "el");
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "en");
         startActivityForResult(intent, REQUEST_SPEECH_RECOGNIZER);
     }
 
